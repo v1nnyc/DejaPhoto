a14f0a41baf66ee925e05bfb4ae8bcba1e957f49
v1nnyc <cannallav019@yahoo.com>
2017-05-09 15:37:46 -0700

    modified method to take parameters

--- app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/wallpaper.java
+++ app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/wallpaper.java
@@ -10,23 +10,28 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class wallpaper extends Activity {
-    Bitmap bitmap;
-    int lastImageRef;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //call method with a drawable id
         changeWallpaper(R.drawable.hello);
     }
 
-    public void changeWallpaper(int icon){
+    /*METHOD CHANGES SYSTEM WALLPAPER
+        call by passing in a resource id
+        changeWallpaper(R.drawable.hello);
+        parameters: int resource, this NEEDS to be an ID for a DRAWABLE
+        picture.
+     */
+    public void changeWallpaper(int resource){
         WallpaperManager myWallpaperManager
                 = WallpaperManager.getInstance(getApplicationContext());
         try {
-            myWallpaperManager.setResource(icon);
+            myWallpaperManager.setResource(resource);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
