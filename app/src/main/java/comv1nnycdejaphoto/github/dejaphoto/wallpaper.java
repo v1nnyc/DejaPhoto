e2646faa5dda6b47e01149f37c88c10aef990809
v1nnyc <cannallav019@yahoo.com>
2017-05-09 15:01:51 -0700

    got wallpaper to change

---
+++ app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/wallpaper.java
@@ -0,0 +1,28 @@
+package comv1nnycdejaphoto.github.dejaphoto;
+
+import java.io.IOException;
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class wallpaper extends Activity {
+    Bitmap bitmap;
+    int lastImageRef;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        WallpaperManager myWallpaperManager
+                = WallpaperManager.getInstance(getApplicationContext());
+        try {
+            myWallpaperManager.setResource(R.drawable.hello);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
