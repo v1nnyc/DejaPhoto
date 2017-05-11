6d6ffba18ecd54fd2c62cd528db58118ba35d044
simon678910 <s5hui@ucsd.edu>
2017-05-11 01:15:15 -0700

    Widget button works, but need to add stuff

--- app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/DejaWidget.java
+++ app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/DejaWidget.java
@@ -5,8 +5,11 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,24 +17,42 @@ import java.util.Random;
  * Implementation of App Widget functionality.
  */
 public class DejaWidget extends AppWidgetProvider {
+    private static final String MyOnClick = "myOnClickTag";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+    void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                         int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.deja_widget);
+        views.setOnClickPendingIntent(R.id.release, getPendingSelfIntent(context, MyOnClick));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
-        Log.v("error","error");
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
+
         for (int appWidgetId : appWidgetIds) {
+
+            Log.v("Debug","FIRST TIME\n");
+//            RemoteViews remoteViews =  new RemoteViews(context.getPackageName(), R.layout.deja_widget);
+//
+//            Intent intent = new Intent(context,MainActivity.class);
+//
+//            PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+//            remoteViews.setOnClickPendingIntent(R.id.release,pendingIntent);
             updateAppWidget(context, appWidgetManager, appWidgetId);
+
+
+
         }
     }
 
@@ -43,6 +64,20 @@ public class DejaWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (MyOnClick.equals(intent.getAction())) {
+            //your onClick action is here
+            //display in short period of time
+            Toast.makeText(context, "msg msgasdasd", Toast.LENGTH_SHORT).show();
+            wallpaper wp = new wallpaper();
+            wp.changeWallpaper(R.drawable.hello);
+
+
+        }
     }
 }
 
