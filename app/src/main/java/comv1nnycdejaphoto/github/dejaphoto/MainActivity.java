2f6b37486ad0ed60a1eafb97475caff74e51abb6
simon678910 <s5hui@ucsd.edu>
2017-05-11 05:49:11 -0700

    Release button in main added

--- app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/MainActivity.java
+++ app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/MainActivity.java
@@ -30,6 +30,9 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private static Context sContext;
+    /* This will tell the different between choose or release, 1 is for choose, 0 for release*/
+    static final int PICK_CHOOSE = 1 ;
+    static final int PICK_RELEASE = 0 ;
 
     //constructor
     public MainActivity() {
@@ -41,25 +44,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
 
+        /*Ask the permission to read the images*/
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
 
+        /*Button that links to the Choose*/
         Button load_image = (Button) findViewById(R.id.choose);
+        /*onClick Event*/
         load_image.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View v) {
+                  /* Ask user to pick a image and save its uri, make the result become intent*/
                   Intent intent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                  startActivityForResult(intent, 1);
-
+                  /* pass the intent with the option of choose button beign clicked*/
+                  startActivityForResult(intent, PICK_CHOOSE);
               }
         }
         );
 
+        /* Same thing but for the release button*/
+        Button release_image = (Button) findViewById(R.id.release);
+        release_image.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, PICK_RELEASE);
+            }
+        });
+
 
     }
 
-    /*required for other classes to be able to access MainActivity
-     */
+    /*required for other classes to be able to access MainActivity*/
     public static Context getContext() {
         return sContext;
     }
@@ -67,24 +83,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Uri uri = data.getData();
-        try {
-            Bitmap bitmap= MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-            WallpaperManager wallpaperManager=WallpaperManager.getInstance(this);
-            Log.v("in",bitmap.toString());
-            wallpaperManager.setBitmap(bitmap);
-        } catch (IOException e) {
-            Log.v("ERROR","ERROR");
-            e.printStackTrace();
-        }
-//        WallpaperManager wallpaperManager= WallpaperManager.getInstance(this);
-//        try {
-//            wallpaperManager.setBitmap(bitmap);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        /*If user press back while picking images, exit the method */
+        if(data == null)
+            return;
 
+        /*The choose button being clicked*/
+        if(requestCode == PICK_CHOOSE) {
+            /*Get the data as type of Uri*/
+            Uri uri = data.getData();
+            try {
+                /*Bitmap is one type of image, open the uri with bitmap*/
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                /*Change the wallpaper to the loaded bitmap*/
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+                wallpaperManager.setBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        /* Release button being clicked*/
+        if(requestCode == PICK_RELEASE){
+            Uri uri = data.getData();
+            /*pathOfFifle will give you the path of the file*/
+            String pathOfFile = uri.getPath();
+
+            //TODO Realse the path from the queue
+
+            /*Log is for debuging purpose*/
+            Log.v("Path", uri.getPath());
+        }
     }
+
+    /*Get from piazza, original from codepath :
+        https://guides.codepath.com/android/Managing-Runtime-Permissions-with-PermissionsDispatcher */
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
