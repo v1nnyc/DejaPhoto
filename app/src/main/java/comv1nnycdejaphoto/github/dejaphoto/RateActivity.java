4bab7c57a40d458dd9f56f51d6e657ab99a9b1fe
ShirleyLam <hplam@ucsd.edu>
2017-05-14 01:53:19 -0700

    Task 10.3 method to change display mode

--- app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/RateActivity.java
+++ app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/RateActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,41 +18,63 @@ public class RateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        sharedPreferences = getSharedPreferences("DejaPhoto",MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("DejaPhoto", MODE_PRIVATE);
         editor = sharedPreferences.edit();
         setContentView(R.layout.rate);
 
         final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         final TextView textView = (TextView) findViewById(R.id.rateView);
-        int progress =  sharedPreferences.getInt("Rate",0);
+        int progress = sharedPreferences.getInt("Rate", 0);
         seekBar.setProgress(progress);
         seekBar.setMax(11);
 
-        textView.setText("Display Rate: " + (seekBar.getProgress()+1)*5);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+        textView.setText("Display Rate: " + (seekBar.getProgress() + 1) * 5);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             /* display the value according to the seekbar */
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textView.setText("Display Rate : " + (progress+1)*5);
-                editor.putInt("Rate",progress+1);
+                textView.setText("Display Rate : " + (progress + 1) * 5);
+                editor.putInt("Rate", progress + 1);
                 editor.apply();
             }
 
             /* display toast msg when user starts changing display rate */
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(RateActivity.this,"Changing Display Rate",Toast.LENGTH_LONG).show();
+                Toast.makeText(RateActivity.this, "Changing Display Rate", Toast.LENGTH_LONG).show();
             }
 
             /* display toast msg when user finished changing display rate */
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(RateActivity.this,"Set Display Rate",Toast.LENGTH_LONG).show();
+                Toast.makeText(RateActivity.this, "Set Display Rate", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checked) {
+                if (checked == R.id.time) {
+                    editor.putString("Mode","time");
+                    Toast.makeText(getApplicationContext(), "Set mode: Time", Toast.LENGTH_SHORT).show();
+                } else if (checked == R.id.day) {
+                    editor.putString("Mode","day");
+                    Toast.makeText(getApplicationContext(), "Set mode: Day", Toast.LENGTH_SHORT).show();
+                } else {
+                    editor.putString("Mode","week");
+                    Toast.makeText(getApplicationContext(), "Set mode: Week", Toast.LENGTH_SHORT).show();
+                }
+                editor.apply();
             }
 
         });
     }
+
+
 
     @Override
     public void onBackPressed()
@@ -60,5 +83,7 @@ public class RateActivity extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
+
+
 
 }
