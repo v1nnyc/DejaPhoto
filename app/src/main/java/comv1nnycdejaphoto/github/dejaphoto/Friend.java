66009804dedc968690a28adaa8572911107da88c
v1nnyc <cannallav019@yahoo.com>
2017-05-31 18:15:09 -0700

    added interfaces and classes

---
+++ app/src/main/java/comv1nnycdejaphoto/github/dejaphoto/Friend.java
@@ -0,0 +1,11 @@
+package comv1nnycdejaphoto.github.dejaphoto;
+
+/**
+ * Created by VINCENT on 5/31/17.
+ */
+
+public interface Friend {
+    public void add(String Id);
+    public void remove(String Id);
+    public boolean handleRequest();
+}
